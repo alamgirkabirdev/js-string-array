23d2803e5d6595c6c04e59006aef69c6ec133b10
Alamgir kabir <alamgir.saguli@gmail.com>
2022-10-05 10:00:34 +0600

    first commit of js srting array

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+const userInput = 'blACkpinK';
+const savedUserName = 'blackpink';
+// console.log(userInput.toLocaleLowerCase());
+// console.log(savedUserName.toLocaleLowerCase);
+if(userInput.toLocaleLowerCase == savedUserName.toLocaleLowerCase){
+    console.log('user exists');
+}
